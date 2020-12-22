9a4f7d01daa5ddca615f793fc238f1ee80792688
shaobo.chen <shaobo.chen@chaitin.com>
2020-12-22 23:26:09 +0800

    feat: pagenation add logic

--- web/src/views/Photo.vue
+++ web/src/views/Photo.vue
@@ -12,6 +12,7 @@
                 <div class="card progressive">
                     <a class="mark" href="/photo/BractCloseup_ZH-CN9096611979?force=home_1"></a>
                     <img class="" :src="photo.img_path">
+<!--                    <img src="https://magicstyle.fun/_/photo/80910536-c5d2-4a6f-8308-faeac16d6b5b">-->
                     <div class="description"><h3>photo:{{photo.id}}</h3></div>
                 </div>
             </div>
@@ -19,13 +20,12 @@
 
         <div class="photo-footer">
             <ul class="pagination">
-                <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                <li class="active"><a href="#!">1</a></li>
-                <li class="waves-effect"><a href="#!">2</a></li>
-                <li class="waves-effect"><a href="#!">3</a></li>
-                <li class="waves-effect"><a href="#!">4</a></li>
-                <li class="waves-effect"><a href="#!">5</a></li>
-                <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                <li @click="getPhoto(activePage-1)" v-bind:class="{disabled:activePage<=1}"
+                    ><a><i class="material-icons">chevron_left</i></a></li>
+                <li @click="getPhoto(index)" v-for="index of Math.ceil(total/count)" :key="index"
+                    v-bind:class="{active:index==activePage}" class="waves-effect"><a>{{index}}</a></li>
+                <li @click="getPhoto(activePage+1)" v-bind:class="{disabled:activePage>=Math.ceil(total/count)}"
+                    class="waves-effect"><a><i class="material-icons">chevron_right</i></a></li>
             </ul>
             <div class="extra-info">
                 <span> ©2020 七色彩云</span>
@@ -49,15 +49,24 @@
             category: [],
 
             offset: 0,
-            count: 15,
+            count: 30,
+            activePage: 1,
         }),
         created() {
             this.getPhoto()
             this.getCategory()
         },
         methods: {
-            getPhoto() {
-                GetPhotoListAPI({"count": this.count, "offset": this.offset}).then(resp => {
+            getPhoto(page=1) {
+                if(page < 1){
+                    page = 1
+                }
+                let maxpage = Math.ceil(this.total/this.count)
+                if(page > maxpage) {
+                    page = maxpage
+                }
+                this.activePage = page
+                GetPhotoListAPI({"count": this.count, "offset": (page-1)*this.count}).then(resp => {
                     this.photos = resp.data.items
                     this.total = resp.data.total
                 })
