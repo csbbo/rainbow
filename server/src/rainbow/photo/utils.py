f236053adf81b926cfe878cf8c8ed250364d3dba
csbbo <chshbox@gmail.com>
2021-01-19 17:22:18 +0800

    add func to_cartoon

--- server/src/rainbow/photo/utils.py
+++ server/src/rainbow/photo/utils.py
@@ -12,7 +12,7 @@ def get_image_dpi(name, path=settings.PHOTOS_PATH):
 
 
 def cv2_base64(image):
-    base64_str = cv2.imencode('.jpg',image)[1].tobytes()
+    base64_str = cv2.imencode('.jpg', image)[1].tobytes()
     base64_str = base64.b64encode(base64_str)
     return base64_str
 
@@ -33,3 +33,33 @@ def to_sketch(image):
 
     img_opening_blurred = cv2.GaussianBlur(img_opening, (3, 3), 0)
     return img_opening_blurred
+
+
+def to_cartoon(picture_name, output_name):
+    num_down = 2  # 缩减像素采样的数目
+    num_bilateral = 7  # 定义双边滤波的数目
+    img_rgb = cv2.imread(picture_name)  # 读取图片
+    # 用高斯金字塔降低取样
+    img_color = img_rgb
+    for _ in range(num_down):
+        img_color = cv2.pyrDown(img_color)
+    # 重复使用小的双边滤波代替一个大的滤波
+    for _ in range(num_bilateral):
+        img_color = cv2.bilateralFilter(img_color, d=9, sigmaColor=9, sigmaSpace=7)
+    # 升采样图片到原始大小
+    for _ in range(num_down):
+        img_color = cv2.pyrUp(img_color)
+    # 转换为灰度并且使其产生中等的模糊
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+    img_blur = cv2.medianBlur(img_gray, 7)
+    # 检测到边缘并且增强其效果
+    img_edge = cv2.adaptiveThreshold(img_blur, 255,
+                                     cv2.ADAPTIVE_THRESH_MEAN_C,
+                                     cv2.THRESH_BINARY,
+                                     blockSize=9,
+                                     C=2)
+    # 转换回彩色图像
+    img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
+    img_cartoon = cv2.bitwise_and(img_color, img_edge)
+    # 保存转换后的图片
+    cv2.imwrite(output_name, img_cartoon)
