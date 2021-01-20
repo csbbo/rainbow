35e9391799353fb82a3e6091e8732d02d370d97d
csbbo <chshbox@gmail.com>
2021-01-20 15:21:47 +0800

    temp main page

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import './common/axios'
 import router from './router'
 
 Vue.config.productionTip = false
--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -19,7 +19,34 @@
           </div>
         </nav>
 
-       <footer class="page-footer">
+          <div class="carousel carousel-slider">
+            <a class="carousel-item" href="#one!"><img src="/_/photo/22020d390bbfcadb60e89a2d0122dc67"></a>
+            <a class="carousel-item" href="#two!"><img src="/_/photo/8a03d0ebcd49714266b5c55676e9bc17"></a>
+          </div>
+
+<!--          <div class="carousel carousel-slider center">-->
+<!--            <div class="carousel-fixed-item center">-->
+<!--              <a class="btn waves-effect white grey-text darken-text-2">button</a>-->
+<!--            </div>-->
+<!--            <div class="carousel-item red white-text" href="#one!">-->
+<!--              <h2>First Panel</h2>-->
+<!--              <p class="white-text">This is your first panel</p>-->
+<!--            </div>-->
+<!--            <div class="carousel-item amber white-text" href="#two!">-->
+<!--              <h2>Second Panel</h2>-->
+<!--              <p class="white-text">This is your second panel</p>-->
+<!--            </div>-->
+<!--            <div class="carousel-item green white-text" href="#three!">-->
+<!--              <h2>Third Panel</h2>-->
+<!--              <p class="white-text">This is your third panel</p>-->
+<!--            </div>-->
+<!--            <div class="carousel-item blue white-text" href="#four!">-->
+<!--              <h2>Fourth Panel</h2>-->
+<!--              <p class="white-text">This is your fourth panel</p>-->
+<!--            </div>-->
+<!--          </div>-->
+
+        <footer class="page-footer">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
@@ -56,6 +83,16 @@
             window.jQuery(document).ready(function(){
                 window.jQuery(".dropdown-trigger").dropdown();
             });
+
+            window.jQuery('.carousel.carousel-slider').carousel({
+                fullWidth: true,
+                indicators: true,
+                numVisible: 2,
+            });
         },
     }
 </script>
+
+<style scoped>
+
+</style>
