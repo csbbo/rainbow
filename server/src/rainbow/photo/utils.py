9ad83be30acbdc37b131edf9613f1d85ec93051a
csbbo <chshbox@gmail.com>
2021-01-29 18:01:48 +0800

    to cartoon image name change

--- server/src/rainbow/photo/utils.py
+++ server/src/rainbow/photo/utils.py
@@ -34,13 +34,12 @@ def to_sketch(image):
     img_opening_blurred = cv2.GaussianBlur(img_opening, (3, 3), 0)
     return img_opening_blurred
 
-# have some problem
-def to_cartoon(image):
+
+def cartoonise(image):
     num_down = 2  # 缩减像素采样的数目
     num_bilateral = 7  # 定义双边滤波的数目
-    img_rgb = image
     # 用高斯金字塔降低取样
-    img_color = img_rgb
+    img_color = image
     for _ in range(num_down):
         img_color = cv2.pyrDown(img_color)
     # 重复使用小的双边滤波代替一个大的滤波
@@ -50,7 +49,7 @@ def to_cartoon(image):
     for _ in range(num_down):
         img_color = cv2.pyrUp(img_color)
     # 转换为灰度并且使其产生中等的模糊
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+    img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     img_blur = cv2.medianBlur(img_gray, 7)
     # 检测到边缘并且增强其效果
     img_edge = cv2.adaptiveThreshold(img_blur, 255,
